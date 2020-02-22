896e06ba7d90546dfa4312b1490c41a4247800c1
dddxing <33138956+dddxing@users.noreply.github.com>
2020-02-22 14:50:05 -0500

    Add files via upload

---
+++ Project1/build/CMakeFiles/wm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wm.dir/script/welcome_message.cpp.o"
+  "libwm.pdb"
+  "libwm.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
